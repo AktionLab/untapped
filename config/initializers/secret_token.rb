f5df01a5575cb9dbe66ce6e462bfa4f9c4af7621
Daniel <daniel.zukowski@gmail.com>
2012-10-30 11:43:05 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Untapped::Application.config.secret_token = '3f817708f0a18efb2b8e3edcc6886cd1977356c6f93bc1782b1e440e4c3a802be6c90dee918157c07cd215b46a1988ae0641607fdce7a39ec48f85577b924912'
