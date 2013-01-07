02117e7dfc9501c5be520e56856710fe17dcb195
Patrick <patrick@aktionlab.com>
2013-01-06 20:29:19 -0700

    Worked on devise and installed active admin just in case.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Untapped::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   devise_for :users
 
   root :to => "pages#show", :id => 'home'
