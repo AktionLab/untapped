e6e153ca1b804d79c91f003f48f6acf3e156e257
Patrick <patrick@aktionlab.com>
2013-01-06 22:19:13 -0700

    Added fields in database for user profile. Also tried getting user profiles to display

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Untapped::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-
   devise_for :users
 
   root :to => "pages#show", :id => 'home'
   get '/:id', :to => 'pages#show', :as => :pages
+
+  resources :users
 end
